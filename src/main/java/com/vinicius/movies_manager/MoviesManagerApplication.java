51381e36942f5df10f99e6146af18e91643a242d
Vinicius <vini456nascimento@gmail.com>
2024-02-06 10:55:37 -0300

    feat: init app

---
+++ src/main/java/com/vinicius/movies_manager/MoviesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.movies_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinicius/movies_manager/MoviesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.movies_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
